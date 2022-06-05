c4c161de5ae2a517dfea995d52c7e607265732c2
Amit Singh <99385950+its-amit-1998@users.noreply.github.com>
2022-06-05 17:31:48 +0530

    Add files via upload

---
+++ email.php
@@ -0,0 +1,15 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<?php 
+	mail($_POST[email], 'contact', $_POST[message]);
+?>
+<p>Your email has been send.</p>
+</body>
+</html>
+
